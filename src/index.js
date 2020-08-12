3a4256e423a3b7f4f1d7c62acdfd8f39abdb21a6
a0g00sl <ayush.gupta@walmartlabs.com>
2020-08-12 20:29:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('react')
+// );
+
+const javascript = () => {
+  document.getElementById('javascript').innerHTML = 
+  `<div className="App">
+    <div className="date-component">
+      ${new Date().toString()}
+    </div>
+  </div>`;
+}
+
+const react = () => {
+  ReactDOM.render(<App/>,document.getElementById('react'));
+}
+
+// react();
+
+setInterval(()=>{
+  react();
+  javascript();
+},1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
